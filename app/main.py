700c867f1fb71eabc4225b2888e233c67f3bd762
pwnd27 <thebuff27@mail.ru>
2023-04-08 02:28:09 +0700

    add test create user

--- app/main.py
+++ app/main.py
@@ -4,13 +4,3 @@ from app import schemas
 
 app = FastAPI()
 app.include_router(users.router, tags=['Users'], prefix='/users')
-
-
-@app.get('/')
-async def root():
-    return {'message': 'Hello friend!'}
-
-
-@app.post('/')
-async def postroot(user: schemas.UserBase):
-    return user
--- app/tests/conftest.py
+++ app/tests/conftest.py
@@ -5,28 +5,28 @@ from sqlalchemy.orm import sessionmaker
 from app.models import Base, User
 
 
-@pytest.fixture(scope='function')
-async def db_session():
+# @pytest.fixture(scope='function')
+# async def db_session():
 
-    engine = create_async_engine('sqlite+aiosqlite:///:memory:', echo=True)
+#     engine = create_async_engine('sqlite+aiosqlite:///:memory:', echo=True)
     
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
+#     async with engine.begin() as conn:
+#         await conn.run_sync(Base.metadata.create_all)
 
-    async_session = sessionmaker(
-        engine, expire_on_commit=False, class_=AsyncSession
-    )
+#     async_session = sessionmaker(
+#         engine, expire_on_commit=False, class_=AsyncSession
+#     )
 
-    async with async_session() as session:
-        yield session
+#     async with async_session() as session:
+#         yield session
 
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.drop_all)
+#     async with engine.begin() as conn:
+#         await conn.run_sync(Base.metadata.drop_all)
 
 
-@pytest.fixture(scope='function')
-async def user(db_session):
-    user = User(email='test@mail.ru', hashed_password='qwerty123')
-    db_session.add(user)
-    await db_session.flush()
-    return user
+# @pytest.fixture(scope='function')
+# async def user(db_session):
+#     user = User(email='test@mail.ru', hashed_password='qwerty123')
+#     db_session.add(user)
+#     await db_session.flush()
+#     return user
