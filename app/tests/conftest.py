66ffc524fe3d5624e91f052b82ec930765fb187c
pwnd27 <thebuff27@mail.ru>
2023-04-22 19:59:53 +0700

    add tests auth

--- app/tests/conftest.py
+++ app/tests/conftest.py
@@ -8,7 +8,7 @@ from app.api.models import Base, User
 
 
 @pytest_asyncio.fixture(scope='function')
-async def db_session():
+async def session():
 
     engine = create_async_engine('sqlite+aiosqlite:///:memory:', echo=True, future=True)
     
@@ -19,23 +19,37 @@ async def db_session():
         engine, expire_on_commit=False, class_=AsyncSession # pyright: ignore
     )
 
-    async with async_session() as session:
-        session.add(User(email='test@mail.ru', hashed_password='qwerty123'))
-        await session.flush()
-        yield session
+    async with async_session() as test_session:
+        yield test_session
 
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.drop_all)
 
 
 @pytest_asyncio.fixture(scope='function')
+async def user(session):
+    test_user = User(
+        email='test@mail.ru', 
+        hashed_password='$2b$12$EAxARHzgjkvqxwZtLhjwYO14skTGQKU68RWuTHnVfH46Uve6TN9Ry' # pass 'qwerty123'
+    ) 
+    session.add(test_user)
+    await session.commit()
+    yield test_user
+
+
+@pytest_asyncio.fixture(scope='function')
 async def client():
     async with AsyncClient(app=app, base_url="http://test") as test_client:
         yield test_client
+        
+
+@pytest_asyncio.fixture(scope='function')
+async def user_cookies(user, client):
+    response = await client.post('/auth/login', json={
+        'email': 'test@mail.ru',
+        'password': 'testtest',
+    })
+    yield response.cookies
 
 # @pytest_asyncio.fixture(scope='function')
-# async def user(db_session):
-#     user = User(email='test@mail.ru', hashed_password='qwerty123')
-#     db_session.add(user)
-#     await db_session.flush()
-#     return user
+# async def user_headers()
--- app/tests/test_routes_auth.py
+++ app/tests/test_routes_auth.py
@@ -1,36 +1,125 @@
-# import pytest
-# from app.api import service
-# from app.api import models
+import pytest
+from httpx import Cookies
+from fastapi import status
+from app.api import service
+from app.api import models
 
 
-# @pytest.mark.asyncio
-# async def test_register_should_return_error_400_when_user_exist(monkeypatch, client) -> None:
-#     data = {'email': 'test12@mail.ru', 'password': 'testtest', 'password_confirm': 'testtest'}
+@pytest.mark.asyncio
+async def test_register_should_return_error_409_when_user_exist(monkeypatch, client, user) -> None:
+    data = {
+        'email': 'test@mail.ru', 
+        'password': 'testtest', 
+        'password_confirm': 'testtest'
+    }
  
-#     async def mock_get_user(email, session) -> models.User:
-#         return models.User(email='test12@mail.ru', hashed_password='testtest')
+    async def mock_get_user(email, session) -> models.User:
+        return models.User(email=data['email'], hashed_password=data['password'])
  
-#     monkeypatch.setattr(service, 'get_user', mock_get_user)
+    monkeypatch.setattr(service, 'get_user', mock_get_user)
 
-#     response = await client.post("/users/signup", json=data)
-#     assert response.status_code == 400
+    response = await client.post('/auth/signup', json=data)
+    assert response.status_code == status.HTTP_409_CONFLICT
 
 
-# @pytest.mark.asyncio
-# async def test_register_when_not_exist_in_db(monkeypatch, client) -> None:
-#     data = {'email': 'test12@mail.ru', 'password': 'testtest', 'password_confirm': 'testtest'}
+@pytest.mark.asyncio
+async def test_register_when_not_exist_in_db(monkeypatch, client) -> None:
+    data = {
+        'email': 'test12@mail.ru', 
+        'password': 'testtest', 
+        'password_confirm': 'testtest'
+    }
     
-#     async def mock_get_user(email, session) -> None:
-#         return None
+    async def mock_get_user(email, session) -> None:
+        return None
  
-#     monkeypatch.setattr(service, 'get_user', mock_get_user)
+    monkeypatch.setattr(service, 'get_user', mock_get_user)
     
-#     async def mock_create_user(user, session) -> models.User:
-#         return models.User(email=user.email, hashed_password='testtest')
+    async def mock_create_user(user, session) -> models.User:
+        return models.User(email=data['email'], hashed_password=data['password'])
  
-#     monkeypatch.setattr(service, 'create_user', mock_create_user)
+    monkeypatch.setattr(service, 'create_user', mock_create_user)
 
-#     response = await client.post("/users/signup", json=data)
-#     assert response.status_code == 201
-#     assert response.json()['email'] == data['email']
+    response = await client.post('/auth/signup', json=data)
+    assert response.status_code == status.HTTP_201_CREATED
+    assert response.json()['email'] == data['email']
+    
+
+@pytest.mark.asyncio
+async def test_register_when_passwords_no_match(client) -> None:
+    data = {
+        'email': 'test12@mail.ru', 
+        'password': 'testtest', 
+        'password_confirm': 'test'
+    }
+    
+    response = await client.post('/auth/signup', json=data)
+    assert response.status_code == status.HTTP_400_BAD_REQUEST
+    assert response.json()['detail'] == 'Пароли не совпадают'
+    
         
+@pytest.mark.asyncio
+async def test_login_when_user_not_exist_in_db(monkeypatch, client) -> None:
+    data = {'email': 'test@mail.ru', 'password': 'testtest'}
+    
+    async def mock_get_user(email, session) -> None:
+        return None
+    
+    monkeypatch.setattr(service, 'get_user', mock_get_user)
+    
+    response = await client.post('/auth/login', json=data)
+    assert response.status_code == status.HTTP_401_UNAUTHORIZED
+    assert response.json()['detail'] == 'Неверная почта или пароль'
+    assert response.headers.get('set-cookie') == None
+    
+    
+@pytest.mark.asyncio
+async def test_login_when_user_is_exist_but_wrong_pass(monkeypatch, client, user) -> None:
+    data = {'email': 'test@mail.ru', 'password': 'test_wrong_pass'}
+    
+    async def mock_get_user(email, session) -> models.User:
+        return models.User(email=user.email, hashed_password=user.hashed_password)
+    
+    monkeypatch.setattr(service, 'get_user', mock_get_user)
+    
+    response = await client.post('/auth/login', json=data)
+    assert response.status_code == status.HTTP_401_UNAUTHORIZED
+    assert response.json()['detail'] == 'Неверная почта или пароль'
+    assert response.headers.get('set-cookie') == None
+    
+    
+@pytest.mark.asyncio
+async def test_login_when_user_is_exist_success_sign_in(monkeypatch, client, user) -> None:
+    data = {'email': 'test@mail.ru', 'password': 'qwerty123'}
+    
+    async def mock_get_user(email, session) -> models.User:
+        return models.User(email=user.email, hashed_password=user.hashed_password)
+    
+    monkeypatch.setattr(service, 'get_user', mock_get_user)
+    
+    response = await client.post('/auth/login', json=data)
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json()['msg'] == 'Успешный вход в систему'
+    assert response.headers.get('set-cookie') != None
+    
+    
+@pytest.mark.asyncio
+async def test_refresh_with_cookie(user_cookies, client) -> None:
+    response = await client.get('/auth/refresh', cookies=user_cookies)
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json()['msg'] == 'Токен обновлен'
+    
+    
+@pytest.mark.asyncio
+async def test_refresh_without_cookie(client) -> None:
+    response = await client.get('/auth/refresh')
+    assert response.status_code == status.HTTP_401_UNAUTHORIZED
+    assert response.json()['detail'] == 'Missing cookie refresh_token_cookie'
+
+    
+@pytest.mark.asyncio
+async def test_logout_with_cookie(user_cookies, client) -> None:
+    response = await client.get('/auth/logout', cookies=user_cookies)
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json()['msg'] == 'Успешный выход из системы'
+    
